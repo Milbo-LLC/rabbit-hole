9c741c64b9d33500270fd0d7c478cac314799340
Noah Milberger <n.milberger@gmail.com>
2023-07-08 19:38:12 -0400

    Trying to fix bug with text fields not showing.

--- src/components/feature-course/ui/popups/create-course-popup/CreateCourseForm.tsx
+++ src/components/feature-course/ui/popups/create-course-popup/CreateCourseForm.tsx
@@ -1,21 +1,32 @@
-import Form, { FieldValues } from "@/components/ui/form";
+// import Form, { FieldValues } from "@/components/ui/form";
 import TextField from "@/components/ui/form-fields/TextField";
 import Button from "@/components/ui/buttons/Button";
+import { FieldValues, useForm } from "react-hook-form";
 
 interface CreateCourseFormProps {
   onSubmit: (data: FieldValues) => void;
 }
 
 export default function CreateCourseForm({ onSubmit }: CreateCourseFormProps) {
+  // Form variables
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FieldValues>();
+
   return (
-    <Form className="flex flex-col w-full items-center p-4" onSubmit={onSubmit}>
+    <form className="flex flex-col w-full items-center p-4" onSubmit={onSubmit}>
       <TextField
+        control={control}
         name="title"
         type="text"
         placeholder="Give your course a title"
         required="Title is required."
+        errors={errors}
       />
       <TextField
+        control={control}
         name="description"
         type="text"
         placeholder="Write a short description for your course"
@@ -23,10 +34,11 @@ export default function CreateCourseForm({ onSubmit }: CreateCourseFormProps) {
         multiline
         minRows={4}
         maxRows={4}
+        errors={errors}
       />
       <div className="flex">
         <Button label="Jump in" />
       </div>
-    </Form>
+    </form>
   );
 }
